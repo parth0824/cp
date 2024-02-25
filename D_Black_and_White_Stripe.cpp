601d217e06fe0d4a130ac797b6e1f0a3e1f9a056
parth gupta <parthgupta0824@gmail.com>
2024-02-25 09:55:06 +0530

    d_black

---
+++ D_Black_and_White_Stripe.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n, m, ans = INT_MAX, w = 0, b = 0;
+    cin >> n >> m;
+    string s;
+    cin >> s;
+    for (int i = 0; i < m; i++)
+    {
+        if (s[i] == 'W')
+        {
+            w++;
+        }
+        else
+        {
+            b++;
+        }
+    }
+    ans = min(ans, w);
+    int p = 0;
+    for (int i = m; i < n; i++)
+    { 
+        if (s[p] == 'W')
+        {
+            w--;
+        }
+        else
+        {
+            b--;
+        }
+        if (s[i] == 'W')
+        {
+            w++;
+        }
+        else
+        {
+            b++;
+        }
+        p++;
+        ans = min(ans, w);
+    }
+
+    cout << ans << endl;
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+    return 0;
+}
